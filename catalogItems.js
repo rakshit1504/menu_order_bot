0380f730658b1a6174724ccd85d25ea5764b5db6
Glitch (cute-meteor-spatula) <none>
2025-06-24 12:38:54 +0000

    Updated with Glitch

---
+++ catalogItems.js
@@ -0,0 +1,11 @@
+// catalogItems.js
+export const catalogConfig = {
+  catalog_id: "1446531973159975", 
+  items: [
+    { product_retailer_id: "mojito001" },
+    { product_retailer_id: "pizza001" },
+    { product_retailer_id: "wrap001" },
+    { product_retailer_id: "fries001" },
+    { product_retailer_id: "burger001" }
+  ]
+};
